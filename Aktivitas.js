b60c3c6645d53e260d1e72e5228a8aad06cadbc2
belongs-to-12 <163371978+belongs-to-12@users.noreply.github.com>
2024-04-28 17:17:40 +0700

    Update Aktivitas.js

--- Aktivitas.js
+++ Aktivitas.js
@@ -1,6 +1,22 @@
-var Aktivitas = [
-   ["Liburan", "Candi Cetho, Karanganyar, Jawa Tengah", -7.595801135188305, 111.16806928229686],
-   ["Liburan", "Candi Cetho, Karanganyar, Jawa Tengah", -7.805801135188305, 111.15806928229686],
-   ["Liburan", "Candi Cetho, Karanganyar, Jawa Tengah", -7.55801135188305, 111.15806928229686],
-   ["Liburan", "Candi Cetho, Karanganyar, Jawa Tengah", -7.595801135188305, 111.14806928229686]
-   ]
+const Aktivitas = {
+   {
+      nama     : "Liburan", 
+      lokasi   : "Candi Cetho, Karanganyar, Jawa Tengah", 
+      koordinat: [-7.595801135188305, 111.16806928229686]
+   },
+   {
+      nama: "Liburan", 
+      lokasi :"Candi Cetho, Karanganyar, Jawa Tengah", 
+      koordinat: [-7.805801135188305, 111.15806928229686]
+   },
+   {
+   nama : "Liburan" 
+   lokasi : "Candi Cetho, Karanganyar, Jawa Tengah"
+   koordinat : [-7.55801135188305, 111.15806928229686]
+   },
+   {
+   nama : "Liburan", 
+   lokasi : "Candi Cetho, Karanganyar, Jawa Tengah",
+   koordinat : [-7.595801135188305, 111.14806928229686]
+   }}
+
