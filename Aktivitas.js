ebdc1d7f583e03494cb4d2f7d5f23177adfb9683
belongs-to-12 <163371978+belongs-to-12@users.noreply.github.com>
2024-04-28 23:27:15 +0700

    Update Aktivitas.js

--- Aktivitas.js
+++ Aktivitas.js
@@ -1,22 +1,147 @@
-const Aktivitas = {
+var Aktivitas = {
    {
-      nama     : "Liburan", 
-      lokasi   : "Candi Cetho, Karanganyar, Jawa Tengah", 
-      koordinat: [-7.595801135188305, 111.16806928229686]
+      nama     : "Pemandangan Gedung UGM", 
+      lokasi   : "Rooftop Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada", 
+      koordinat: [-7.773801761021501, 110.3722305164826],
+      foto     : "https://lh3.googleusercontent.com/pw/AP1GczNWd-VgaBkqcqm-oznsS7Rsv8FOlZrpKKqPSa2HBi0RioHBmHI0O3dd0PWAyM41F8AbSG5GG1icgvoYFMrBWWxfow6zbQcMnrEDFbPkZZm0SViJc9it3cYhD60oXRam-zCodxKu2Ow85fggU3ClvGpA=w604-h286-s-no-gm?authuser=0"
    },
    {
-      nama: "Liburan", 
-      lokasi :"Candi Cetho, Karanganyar, Jawa Tengah", 
-      koordinat: [-7.805801135188305, 111.15806928229686]
+      nama      : "Konsolidasi PKM Lolos Pandanaan 2023", 
+      lokasi    :"Ballroom Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada", 
+      koordinat : [-7.773722034246773, 110.37232171158534],
+      foto      : "https://lh3.googleusercontent.com/pw/AP1GczP883-jYMAM7fWZmdP-eJKOElr4_vuig2iiL5rcqI8_FWccW6r6-wsmdyWMhCeOvz_5JE1xVowOeBZwXImbH-1r9FK_pOqpwVvvGeI80MusI51dgW6W9Enc5ZHPrVybsV23gVYvnjVuJoIIBTC9TLkn=w508-h286-s-no-gm?authuser=0"
    },
    {
-   nama : "Liburan" 
-   lokasi : "Candi Cetho, Karanganyar, Jawa Tengah"
-   koordinat : [-7.55801135188305, 111.15806928229686]
+   nama      : "Panorama Laut" 
+   lokasi    : "Pantai Watu Kodok, Gunung Kidul"
+   koordinat : [-8.13694947839775, 110.57428984516388],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczOnJQtx2FiQT3avLW-ouHwq5GpWL8oWkEwcTthoUEgoIXOIrEhw3aJq1Ej7F1CYGJDNpEKywjPzA6P5BSw--izenuvRe4og5sm1MC3ai00bKWedEvqEvUszXnklL7WuNi-ePDKnJWA4V_kDI-ECi6PZ=w161-h286-s-no-gm?authuser=0"
    },
    {
-   nama : "Liburan", 
-   lokasi : "Candi Cetho, Karanganyar, Jawa Tengah",
-   koordinat : [-7.595801135188305, 111.14806928229686]
-   }}
+   nama      : "Potret Bangunan", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Potret Dalam Gedung di Malam Hari", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Gelex 2023", 
+   lokasi    : "Lapangan Pancasila, Universitas Gadjah Mada",
+   koordinat : [-7.771231893659527, 110.37749569291962],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczO6OtkJD9r1BEwesLkm7zrF_OZPH-tkUYrJHBfA890_l0500c5MqmJoyAGFlu0K7f0cf7SO8MQaiJM2cO67eEyW4npdXz4bEVIbJh2MLbNr7fBtZxIX5cu81pwLuKyWwcXKZd0DOSNKebD_zpXUII9d=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Workshop Laporan Kemajuan PKM 2023", 
+   lokasi    : "Gedung DTHV, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.773565238234169, 110.37395785929392],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczMhCsXkVvzjOBox-2Wk0KAyStZYBzgDcBsACYpqMyDzzb-vKFaDyvPJwyiWpUtQoAF4gNDJhvR6YBeJR4yFJ0I2JhXqj4Oz56IWanH_TTxHS1ARwR2WXDfrmtFd-lDbNlAW778-cClxsZmJwJu1hEgN=w604-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Senja di TILC", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7737353220410705, 110.37219832995515],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPE9pDbm_gTP8cqFL_mCnpH-aWTj4DSvmZlC3LZYGfGnX0LMD83pKYjxzAdmhlSZCKKaqiGOs6XO2tieaE-batv7CuO8khq8aquXC9Hy24hzS0xIeNEMcJGWByTCXqiqHS4h8YanOBiFfapuy_fG39n=w161-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Bukit", 
+   lokasi    : "Wisdom Park, Universitas Gadjah Mada",
+   koordinat : [-7.769906122975674, 110.38299685958424],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczM3jF6J9a4HQCPrBjxS_GI4gMSIKM2LgF59y-6Jf45-Ef5UN-l6qHoOrouE99M_xvgqs05aewEN-qVjQmMkGrZt3VqEYRcNSXDFZOXcI5spizhHX-765TdaNEOhyulys5RwA-YHFgAaHSzpk1NBt2CW=w161-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Kapal Waduk Sermo", 
+   lokasi    : "Waduk Sermo, Hargowilis, Kulon Progo, DIY",
+   koordinat : [-7.82593232937508, 110.11662661168153],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczND04s4r7GWSAQCYzYKW7wjQfw5sZU5foSJz96lcP4Ke3fk8GVqHrbF_Abnk87Vs9iZBybGvJNID1R5ASPxn6Wo3CAI0k0dXnun8JqvgssaAWAWJGDi5YMrGCTtTxZCIboTeu8-jU0bUXto4Gdp0Hvs=w161-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Lemari Mushola TILC", 
+   lokasi    : "Mushola Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.773785815661547, 110.37222783427916],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczN_E-9-Z9PC8Rr8ypYtX1Le3g2DKcpOeMaxEl8vuLxTlfnu9Oyk0ydEPYUUia50OlzQ4gmk8rls-RFVpuISsu5e8IWIH7OL0HPw7WpsEc-fRd_9WPPOlAzN5aI3OI4dQCK2SaSpvZlcAQtoovkdj-IJ=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Pengamatan GNSS", 
+   lokasi    : "Gedung DTS, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczM1eADuVhuI5pJVwMyhZjR8FlTr_6mOvSeyKT5kElmsYDRzf2SislXKnT6nKQp8bKmzOM3OPqlIOGOrxMjtrEmQSTFcDdYbUiGHhYp-ANYs60ItCuSPThOtbLRBc2e4zcvIwvZiKR82JkBkrAlu3cfQ=w136-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Praktikum Survei Terestrial", 
+   lokasi    : "Gedung DTK, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.774807768662436, 110.37441621543641],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczOkbVueFwzEVbqmMn08DyhWMT-UKyW3roSpRxXW9tIstbY85T8YtXa-7kn__xuLkBnMzkhzPFrL4Ng85yzj0xjePKIhuiQ68wJ8gFgT1x84Pj79mgF1ttV_xGE5d8dnC4KsimGhpb70YHyoXL9JSzCA=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Kabuma Ramadhan", 
+   lokasi    : "Gedug DEB, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.774740001077486, 110.38012529735437],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczMB45IEd9sjwK4V7P70XsF1PGrOQLs90oAmlwFPBmJZrsmCxbgZssufcMyj9EArtQ1XmoOJyoJ-C9Wss-4ggFsorx0eC9YHJLMs5waxdBg6FAEO_qXYNk8pwqAI68lGNyrsNAVBeSM9BzCm7YYL-BPI=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Pojok Bangunan", 
+   lokasi    : "Kod Mas Gadung, Sleman, DIY",
+   koordinat : [-7.755593223993202, 110.37985305314778],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPIXUlgpdrk45UEUzBR6XPQUia-ThqI4BDQw4BNgaBWdZ6EhYsv8qdMNOL5PEROY7RdDBZXYvCjSdxUUq3689vR3qLmlPTUQGSTHlzHMqergerbstFzLaSXJ0N9es06jh7_SxrMBliw4y3VaPIoSyCJ=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Bukit Botorono", 
+   lokasi    : "Kledung, Temanggung, Jawa Tengah",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczODXUGKL6uxf-3xsNNeS7K4mW4UPrDqbfy1SS_F3pFYu7_MMiFuoEKetSbOzdEcZhwAOy90cB6QeuqIsMe6zOtXenNNxRUPGtWAjET8sfSDNupxecKKy0Oy5k5aI_i514MXuCPoRkSn9ajjfDkf4_rU=w136-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Potret Bangunan", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Potret Bangunan", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Potret Bangunan", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Potret Bangunan", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Potret Bangunan", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Potret Bangunan", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Potret Bangunan", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Potret Bangunan", 
+   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   koordinat : [-7.7738389668523125, 110.37212322819123],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   },
+   }
 
