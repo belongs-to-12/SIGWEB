80201b2fe5e69e380a66b1646e861bb85837da2f
belongs-to-12 <163371978+belongs-to-12@users.noreply.github.com>
2024-04-29 12:10:16 +0700

    Update Aktivitas.js

--- Aktivitas.js
+++ Aktivitas.js
@@ -2,146 +2,162 @@ var Aktivitas = {
    {
       nama     : "Pemandangan Gedung UGM", 
       lokasi   : "Rooftop Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada", 
+      tanggal  : "21 Juni 2023",
       koordinat: [-7.773801761021501, 110.3722305164826],
       foto     : "https://lh3.googleusercontent.com/pw/AP1GczNWd-VgaBkqcqm-oznsS7Rsv8FOlZrpKKqPSa2HBi0RioHBmHI0O3dd0PWAyM41F8AbSG5GG1icgvoYFMrBWWxfow6zbQcMnrEDFbPkZZm0SViJc9it3cYhD60oXRam-zCodxKu2Ow85fggU3ClvGpA=w604-h286-s-no-gm?authuser=0"
    },
    {
       nama      : "Konsolidasi PKM Lolos Pandanaan 2023", 
-      lokasi    :"Ballroom Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada", 
+      lokasi    : "Ballroom Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada", 
+      tanggal   : "24 Juni 2023",
       koordinat : [-7.773722034246773, 110.37232171158534],
       foto      : "https://lh3.googleusercontent.com/pw/AP1GczP883-jYMAM7fWZmdP-eJKOElr4_vuig2iiL5rcqI8_FWccW6r6-wsmdyWMhCeOvz_5JE1xVowOeBZwXImbH-1r9FK_pOqpwVvvGeI80MusI51dgW6W9Enc5ZHPrVybsV23gVYvnjVuJoIIBTC9TLkn=w508-h286-s-no-gm?authuser=0"
    },
    {
-   nama      : "Panorama Laut" 
-   lokasi    : "Pantai Watu Kodok, Gunung Kidul"
+   nama      : "Panorama Laut",
+   lokasi    : "Pantai Watu Kodok, Gunung Kidul",
+   tanggal   : "25 Juni 2023", 
    koordinat : [-8.13694947839775, 110.57428984516388],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczOnJQtx2FiQT3avLW-ouHwq5GpWL8oWkEwcTthoUEgoIXOIrEhw3aJq1Ej7F1CYGJDNpEKywjPzA6P5BSw--izenuvRe4og5sm1MC3ai00bKWedEvqEvUszXnklL7WuNi-ePDKnJWA4V_kDI-ECi6PZ=w161-h286-s-no-gm?authuser=0"
    },
    {
-   nama      : "Potret Bangunan", 
-   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
-   koordinat : [-7.7738389668523125, 110.37212322819123],
-   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
-   },
-   {
    nama      : "Potret Dalam Gedung di Malam Hari", 
    lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   tanggal   : "7 Agustus 2023", 
    koordinat : [-7.7738389668523125, 110.37212322819123],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Gelex 2023", 
    lokasi    : "Lapangan Pancasila, Universitas Gadjah Mada",
+   tanggal   : "18 Agustus 2023", 
    koordinat : [-7.771231893659527, 110.37749569291962],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczO6OtkJD9r1BEwesLkm7zrF_OZPH-tkUYrJHBfA890_l0500c5MqmJoyAGFlu0K7f0cf7SO8MQaiJM2cO67eEyW4npdXz4bEVIbJh2MLbNr7fBtZxIX5cu81pwLuKyWwcXKZd0DOSNKebD_zpXUII9d=w286-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Workshop Laporan Kemajuan PKM 2023", 
    lokasi    : "Gedung DTHV, Sekolah Vokasi, Universitas Gadjah Mada",
+   tanggal   : "9 September 2023", 
    koordinat : [-7.773565238234169, 110.37395785929392],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczMhCsXkVvzjOBox-2Wk0KAyStZYBzgDcBsACYpqMyDzzb-vKFaDyvPJwyiWpUtQoAF4gNDJhvR6YBeJR4yFJ0I2JhXqj4Oz56IWanH_TTxHS1ARwR2WXDfrmtFd-lDbNlAW778-cClxsZmJwJu1hEgN=w604-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Senja di TILC", 
    lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   tanggal   : "12 September 2023", 
    koordinat : [-7.7737353220410705, 110.37219832995515],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczPE9pDbm_gTP8cqFL_mCnpH-aWTj4DSvmZlC3LZYGfGnX0LMD83pKYjxzAdmhlSZCKKaqiGOs6XO2tieaE-batv7CuO8khq8aquXC9Hy24hzS0xIeNEMcJGWByTCXqiqHS4h8YanOBiFfapuy_fG39n=w161-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Bukit", 
    lokasi    : "Wisdom Park, Universitas Gadjah Mada",
+   tanggal   : "8 November 2023", 
    koordinat : [-7.769906122975674, 110.38299685958424],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczM3jF6J9a4HQCPrBjxS_GI4gMSIKM2LgF59y-6Jf45-Ef5UN-l6qHoOrouE99M_xvgqs05aewEN-qVjQmMkGrZt3VqEYRcNSXDFZOXcI5spizhHX-765TdaNEOhyulys5RwA-YHFgAaHSzpk1NBt2CW=w161-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Kapal Waduk Sermo", 
    lokasi    : "Waduk Sermo, Hargowilis, Kulon Progo, DIY",
+   tanggal   : "11 November 2023", 
    koordinat : [-7.82593232937508, 110.11662661168153],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczND04s4r7GWSAQCYzYKW7wjQfw5sZU5foSJz96lcP4Ke3fk8GVqHrbF_Abnk87Vs9iZBybGvJNID1R5ASPxn6Wo3CAI0k0dXnun8JqvgssaAWAWJGDi5YMrGCTtTxZCIboTeu8-jU0bUXto4Gdp0Hvs=w161-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Lemari Mushola TILC", 
    lokasi    : "Mushola Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
+   tanggal   : "3 Mei 2024", 
    koordinat : [-7.773785815661547, 110.37222783427916],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczN_E-9-Z9PC8Rr8ypYtX1Le3g2DKcpOeMaxEl8vuLxTlfnu9Oyk0ydEPYUUia50OlzQ4gmk8rls-RFVpuISsu5e8IWIH7OL0HPw7WpsEc-fRd_9WPPOlAzN5aI3OI4dQCK2SaSpvZlcAQtoovkdj-IJ=w286-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Pengamatan GNSS", 
    lokasi    : "Gedung DTS, Sekolah Vokasi, Universitas Gadjah Mada",
+   tanggal   : "26 Maret 2024", 
    koordinat : [-7.7738389668523125, 110.37212322819123],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczM1eADuVhuI5pJVwMyhZjR8FlTr_6mOvSeyKT5kElmsYDRzf2SislXKnT6nKQp8bKmzOM3OPqlIOGOrxMjtrEmQSTFcDdYbUiGHhYp-ANYs60ItCuSPThOtbLRBc2e4zcvIwvZiKR82JkBkrAlu3cfQ=w136-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Praktikum Survei Terestrial", 
    lokasi    : "Gedung DTK, Sekolah Vokasi, Universitas Gadjah Mada",
+   tanggal   : "28 Maret 2024", 
    koordinat : [-7.774807768662436, 110.37441621543641],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczOkbVueFwzEVbqmMn08DyhWMT-UKyW3roSpRxXW9tIstbY85T8YtXa-7kn__xuLkBnMzkhzPFrL4Ng85yzj0xjePKIhuiQ68wJ8gFgT1x84Pj79mgF1ttV_xGE5d8dnC4KsimGhpb70YHyoXL9JSzCA=w286-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Kabuma Ramadhan", 
    lokasi    : "Gedug DEB, Sekolah Vokasi, Universitas Gadjah Mada",
+   tanggal   : "28 Maret 2024", 
    koordinat : [-7.774740001077486, 110.38012529735437],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczMB45IEd9sjwK4V7P70XsF1PGrOQLs90oAmlwFPBmJZrsmCxbgZssufcMyj9EArtQ1XmoOJyoJ-C9Wss-4ggFsorx0eC9YHJLMs5waxdBg6FAEO_qXYNk8pwqAI68lGNyrsNAVBeSM9BzCm7YYL-BPI=w286-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Pojok Bangunan", 
-   lokasi    : "Kod Mas Gadung, Sleman, DIY",
+   lokasi    : "Kos Mas Gadung, Sleman, DIY",
+   tanggal   : "2 April 2024",
    koordinat : [-7.755593223993202, 110.37985305314778],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczPIXUlgpdrk45UEUzBR6XPQUia-ThqI4BDQw4BNgaBWdZ6EhYsv8qdMNOL5PEROY7RdDBZXYvCjSdxUUq3689vR3qLmlPTUQGSTHlzHMqergerbstFzLaSXJ0N9es06jh7_SxrMBliw4y3VaPIoSyCJ=w286-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Bukit Botorono", 
    lokasi    : "Kledung, Temanggung, Jawa Tengah",
+   tanggal   : "3 April 2024",
    koordinat : [-7.7738389668523125, 110.37212322819123],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczODXUGKL6uxf-3xsNNeS7K4mW4UPrDqbfy1SS_F3pFYu7_MMiFuoEKetSbOzdEcZhwAOy90cB6QeuqIsMe6zOtXenNNxRUPGtWAjET8sfSDNupxecKKy0Oy5k5aI_i514MXuCPoRkSn9ajjfDkf4_rU=w136-h286-s-no-gm?authuser=0"
    },
    {
    nama      : "Potret Bangunan", 
-   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
-   koordinat : [-7.7738389668523125, 110.37212322819123],
+   lokasi    : "Makan Islamn Mendolo, Wonosobo, Jawa Tengah",
+   tanggal   : "3 April 2024",
+   koordinat : [-7.369580122772266, 109.92560980905635],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPrTw5RRHziIrzL9LDCDWDOzDrjGy2Bkhm9Jh0P8zKt40iumsaUWM6sLI4vA4RGyT83NL_t3V3_YrYFIC9jCRcYoFPdGpe3-UASgt15EJcHLzDq4HhXuMVrh-2kI1uWztjBzzfTXvoFwZvel98NKPbI=w136-h286-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Potret Sudut Masjid di Malam Hari", 
+   lokasi    : "Masjid Raya Syeikh Zayed Solo, Surakarta, Jawa Tengah",
+   tanggal   : "8 April 2024",
+   koordinat : [-7.554411662350296, 110.82699951052315],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczNt8XuGgkI2bgSeYo5wxw5LXIl9kngAGsWfVJT7lIf7GVoQ-qMAadhBg0Yg-ep29e3GPH5EzNUMRmorH00JNo8UH42JCSC1KYB6thGJzOuRR3m0AJNmXh96VD41K06tDHppHYEDIVqYsjSvu49XRTFC=w192-h405-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Liburan ke Kawah Sikidang", 
+   lokasi    : "Kawah Sikidang, Banjarnegara, Jawa Tengah",
+   tanggal   : "11 April 2024",
+   koordinat : [-7.220222084742593, 109.90416602920294],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczMbJvFLWlZj0BmZShL1ybCiHOvEdd8be8FiApuPl7VFqqI3XsYPpWn_e8ErA3rjVsLirEWoG8RvMNhkDPBtL1qRqsRF6UXgpuAqT5CWBrxXWWV5MYFbKtO8mdaZK-Wb_T7g-1fELQYcCcD6czpf_ZUI=w228-h405-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Liburan ke Kawah Sikidang", 
+   lokasi    : "Kawah Sikidang, Banjarnegara, Jawa Tengah",
+   tanggal   : "11 April 2024",
+   koordinat : [-7.220317878577129, 109.90367250276464],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczP3h2PDkLWJhnE7ZsMtknZXBXgeGKTUdPr-2hD77FN-jRdWjnq6hjHxxUbY-UwG-P1fN2KanN8wG1Ve7PHJWpANZYupYIXjMx39rhyAggTdRTXhCCFcVmaBDD4y8uDzSLv7YVkd_PJg6rCmRw5nqp2e=w228-h405-s-no-gm?authuser=0"
+   },
+   {
+   nama      : "Liburan ke Kawah Sikidang - Kawah Utama", 
+   lokasi    : "Kawah Sikidang, Banjarnegara, Jawa Tengah",
+   tanggal   : "11 April 2024",
+   koordinat : [-7.220360453614151, 109.90392463042551],
    foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
    },
    {
-   nama      : "Potret Bangunan", 
-   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
-   koordinat : [-7.7738389668523125, 110.37212322819123],
-   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   nama      : "Liburan ke Candi Arjuna", 
+   lokasi    : "Kawasan Candi Dieng, Banjarnegara, Jawa Tengah",
+   tanggal   : "11 April 2024",
+   koordinat : [-7.205034599996251, 109.90691446006883],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczOFZHc6BTqbPYW4mKuzxmm1yFV1BxOcguvYJn8Uuff49heV-sOHC6T0F--gzeqt0IPFsyI-GNCoInd0uNLc7L9ETP0bkjZgiJN5RbmeEEW2NvEhPQfyp2tsqA3U_AX3bqmhXKFCjym_BYrhDp9pdvw8=w228-h405-s-no-gm?authuser=0"
    },
    {
-   nama      : "Potret Bangunan", 
-   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
-   koordinat : [-7.7738389668523125, 110.37212322819123],
-   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
+   nama      : "Panorama Kabut di Siang Hari", 
+   lokasi    : "Bukit Skoter, Banjarnegara, Jawa Tengah",
+   tanggal   : "11 April 2024",
+   koordinat : [-7.19740615087659, 109.90611795067268],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczNqr1z29DnVmNxHKzoDtNtGgMDlz_h2YMs1ibGznZXTmHJxpjx6BSvofp7VLi59zLXEM24dFvFXhrMWDcpI2joVICbDNGmAHPwVMhCmnE7sjHcMpKeWrWHdrPGP1vToawnA-kRsO7nFQweACr32kDU6=w228-h405-s-no-gm?authuser=0"
    },
    {
-   nama      : "Potret Bangunan", 
-   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
-   koordinat : [-7.7738389668523125, 110.37212322819123],
-   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
-   },
-   {
-   nama      : "Potret Bangunan", 
-   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
-   koordinat : [-7.7738389668523125, 110.37212322819123],
-   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
-   },
-   {
-   nama      : "Potret Bangunan", 
-   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
-   koordinat : [-7.7738389668523125, 110.37212322819123],
-   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
-   },
-   {
-   nama      : "Potret Bangunan", 
-   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
-   koordinat : [-7.7738389668523125, 110.37212322819123],
-   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
-   },
-   {
-   nama      : "Potret Bangunan", 
-   lokasi    : "Co-working Space Gedung Teaching Industry Learning Center, Sekolah Vokasi, Universitas Gadjah Mada",
-   koordinat : [-7.7738389668523125, 110.37212322819123],
-   foto      : "https://lh3.googleusercontent.com/pw/AP1GczPY_4VkMRDbtKNCDbQSGWXNeYpHeMCPfVnKXEWc6ZlKx334GW-O83wilYUAqXtmuU5T-jtEackb4FTFFnd3x1rE4hOlutjVJ-T0C_wDGAMiRPTZGTaAjLHc5tZMLFLcZNrOEBribEsXzNbLoqdzwYqz=w286-h286-s-no-gm?authuser=0"
-   },
+   nama      : "Panorama di Sore Hari", 
+   lokasi    : "Bukit Skoter, Banjarnegara, Jawa Tengah",
+   tanggal   : "11 April 2024",
+   koordinat : [-7.197406150883778, 109.90631106973154],
+   foto      : "https://lh3.googleusercontent.com/pw/AP1GczOg_9Q43BCnDK8DNjWMpg0rj7b2qFsf2P0O_d3lnPbeUvRgsck9bZrvmZzgZbe5GIzEHR7r-CZbL1bjSs4dkeFr9PXPd_CzgE7kaJtfhFB3ZMdQusUMPNLKRcP9MOXmq-VergzYexbBuXapTYKzRZGm=w228-h405-s-no-gm?authuser=0"
    }
-
+   }
